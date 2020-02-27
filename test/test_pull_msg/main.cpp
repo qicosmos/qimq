39367171eb50efb0ede040f73dcf4b6ac4dcdcf3
qicosmos <qicosmos@163.com>
2020-02-27 17:09:53 +0800

    add test project

---
+++ test/test_pull_msg/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <rpc_client.hpp>
+#include <chrono>
+#include <fstream>
+#include <codec.h>
+
+using namespace rest_rpc;
+using namespace rest_rpc::rpc_service;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
